9c9f804c02aa616d72eb587c88c3dc8b2be53278
Sameeksha Gupta <136142214+Sameekksshhaa@users.noreply.github.com>
2025-03-23 02:52:20 +0530

    conflict resolved

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,10 @@ import FarmerDashboard from "./pages/FarmerDashboard";
 import BankDashboard from "./pages/BankDashboard";
 import About from "./pages/About";
 import AdminPanel from "./pages/AdminPanel";
-<<<<<<< HEAD
 import ProfileSetup from "./pages/ProfileSetup";
 import ApplyLoan from "./pages/ApplyLoan";
 import CreditScore from "./pages/CreditScore";
 import LoanStatus from "./pages/LoanStatus";
-=======
->>>>>>> f97689c8d221b961df4b431a36c3490332ec9a84
 
 function App() {
   return (
@@ -23,7 +20,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/farmer-login" element={<FarmerLogin />} />
-<<<<<<< HEAD
         <Route path="/bank-login" element={<BankLogin />} />
         <Route path="/farmer-dashboard" element={<FarmerDashboard />} />
         <Route path="/bank-dashboard" element={<BankDashboard />} />
@@ -33,13 +29,6 @@ function App() {
         <Route path="/apply-loan" element={<ApplyLoan />} />
         <Route path="/credit-score" element={<CreditScore />} />
         <Route path="/loan-status" element={<LoanStatus />} />
-=======
-        <Route path="/farmer-dashboard" element={<FarmerDashboard />} />
-        <Route path="/bank-login" element={<BankLogin />} />
-        <Route path="/bank-dashboard" element={<BankDashboard />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/admin" element={<AdminPanel />} />
->>>>>>> f97689c8d221b961df4b431a36c3490332ec9a84
       </Routes>
     </Router>
   );
